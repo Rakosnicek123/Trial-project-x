26f5830b6fa330be0a6a8050255c0a12301f62a4
Rakosnicek123 <Rakosnicek123@gmail.com>
2013-03-04 09:17:30 +0100

    +Hello Git rev1.0-.exe--.cpp

---
+++ C++ kopilátori/Hello Git/Hello Git.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Git\n";   //std::cout je prikaz na tist textu
+    getchar();                    //getchar zastavuje radok a ceká (enter)
+    
+}
